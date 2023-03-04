38d0698728bbde5297ebc0d36f9be9d9ebaf5c25
JustAnonUser <126934101+JustAnonUser@users.noreply.github.com>
2023-03-04 01:37:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,48 @@ chart_data = pd.DataFrame(
     columns=["a", "b", "c"])
 
 st.bar_chart(chart_data)
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+
+st.title('Hey you!...')
+st.title('...')
+st.title('..')
+st.title('.')
+
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('I´m trying to learn this shit')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(2000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Streamlit es raro...')
+data = load_data(2150)
+# Create a text element and let the reader know the data is loading.
+data_load_state2 = st.text('That text was supposed to change...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(2300)
+# Notify the reader that the data was successfully loaded.
+data_load_state2.text('Hope it did it ...    Ahora una gráfica chida')
+
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
