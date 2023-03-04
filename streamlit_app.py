b536b29d9e2209ea3799cacde821d6617aa9a8dd
JustAnonUser <126934101+JustAnonUser@users.noreply.github.com>
2023-03-03 22:17:12 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,11 +33,18 @@ data_load_state2 = st.text('That text was supposed to change...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(16000)
 # Notify the reader that the data was successfully loaded.
-data_load_state2.text('Hope it did it')
+data_load_state2.text('Hope it did it...Now interesting stuff')
+
+
 
 
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
+
+
+curl --request GET 'https://api.apilayer.com/exchangerates_data/live?base=USD&symbols=EUR,GBP' \
+--header 'apikey: bAnevnVZcReKRPYK9w1rZRCB5C1FGmqa'
+
 
 
