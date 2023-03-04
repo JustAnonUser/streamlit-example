2bb843ca0a7b7f31682ec5c9edef4250b5f143b0
JustAnonUser <126934101+JustAnonUser@users.noreply.github.com>
2023-03-03 22:06:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,11 +26,11 @@ data_load_state = st.text('I´m trying to learn this shit')
 data = load_data(50000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Hopefully i´l do it')
-
+data = load_data(10000)
 # Create a text element and let the reader know the data is loading.
 data_load_state2 = st.text('That text was supposed to change...')
 # Load 10,000 rows of data into the dataframe.
-data = load_data(50000)
+data = load_data(15000)
 # Notify the reader that the data was successfully loaded.
 data_load_state2.text('Hope it did it')
 
