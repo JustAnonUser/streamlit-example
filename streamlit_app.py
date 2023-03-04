8a2371af39a70298842f5b91d8faec4b82f3452b
JustAnonUser <126934101+JustAnonUser@users.noreply.github.com>
2023-03-03 22:23:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,14 +33,10 @@ data_load_state2 = st.text('That text was supposed to change...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(16000)
 # Notify the reader that the data was successfully loaded.
-data_load_state2.text('Hope it did it...Now interesting stuff')
-
-
+data_load_state2.text('Hope it did it')
 
 
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
-
-
 
