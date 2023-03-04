f6317e50f2f8c58fbeef247029d55309ee48163a
JustAnonUser <126934101+JustAnonUser@users.noreply.github.com>
2023-03-03 22:21:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,11 +42,5 @@ hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-
-st.area_chart(chart_data)
-
 
 
