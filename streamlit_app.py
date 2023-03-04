831565b2d444543bd7991761df29b8119f595088
JustAnonUser <126934101+JustAnonUser@users.noreply.github.com>
2023-03-04 00:57:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,17 @@ data_load_state = st.text('I´m trying to learn this shit')
 data = load_data(20000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Streamlit es raro...')
-data = load_data(21000)
+data = load_data(21500)
 # Create a text element and let the reader know the data is loading.
 data_load_state2 = st.text('That text was supposed to change...')
 # Load 10,000 rows of data into the dataframe.
-data = load_data(22000)
+data = load_data(23000)
 # Notify the reader that the data was successfully loaded.
-data_load_state2.text('Hope it did it')
+data_load_state2.text('Hope it did it...Ahora una gráfica chida')
+
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
