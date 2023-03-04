24a16c8f0e5451008673e4fa95ed364406e10ffd
JustAnonUser <126934101+JustAnonUser@users.noreply.github.com>
2023-03-03 22:15:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,8 @@ data = load_data(16000)
 data_load_state2.text('Hope it did it')
 
 
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+
